6bfb4af866acd51ab7384bcb63c11d01767b9926
eliasdag <eliasdavid.aguirre.a@gmail.com>
2020-02-01 13:24:39 -0800

    comment out

--- server.js
+++ server.js
@@ -21,17 +21,17 @@ const server = http.createServer((request, response) => {
     //function to retrieve front end sname input
     // const summonerName = getValue(snames);
 
-    //display HTML
-    response.writeHead(200, {'Content-Type': text/html});
-    fs.readFile('./index.html', null, function(error, data) {
-        if (error){
-            response.writeHead(404);
-            response.write("file not found");
-        } else {
-            response.write(data);
-        }
-        reponse.end();
-    });
+    // //display HTML
+    // response.writeHead(200, {'Content-Type': text/html});
+    // fs.readFile('./index.html', null, function(error, data) {
+    //     if (error){
+    //         response.writeHead(404);
+    //         response.write("file not found");
+    //     } else {
+    //         response.write(data);
+    //     }
+    //     reponse.end();
+    // });
     // function to make actual api call 
     makeAPICall(summonerName, response)
 });
